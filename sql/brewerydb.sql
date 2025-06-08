f6b743e0933683a7c0cb9ff2788ac820962772ef
Guilherme Belisario <gbo2000@outlook.com>
2025-06-08 02:12:13 -0300

    .

--- sql/brewerydb.sql
+++ sql/brewerydb.sql
@@ -1,4 +1,3 @@
-
 SET statement_timeout = 0;
 SET lock_timeout = 0;
 SET client_encoding = 'UTF8';
@@ -7,8 +6,6 @@ SET check_function_bodies = false;
 SET client_min_messages = warning;
 SET default_tablespace = '';
 SET default_with_oids = false;
-
-
 
 -- Adicionando extensão para gerar UUID
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
@@ -19,13 +16,13 @@ CREATE TABLE IF NOT EXISTS dim_brewery_type (
     brewery_type VARCHAR(50) NOT NULL
 );
 
--- Criação da tabela dim_brewery 
+-- Criação da tabela dim_brewery
 CREATE TABLE IF NOT EXISTS dim_brewery (
     brewery_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     name VARCHAR(255) NOT NULL,
     brewery_type_id UUID REFERENCES dim_brewery_type(type_id),
     phone VARCHAR(50),
-    is_phone_missing CHAR(1),
+    is_phone_missing BOOLEAN,
     fixed_phone VARCHAR(50),
     website_url VARCHAR(255)
 );
@@ -40,10 +37,9 @@ CREATE TABLE IF NOT EXISTS dim_location (
     state VARCHAR(100),
     zip_code VARCHAR(20),
     country VARCHAR(100),
-    longitude FLOAT,
-    latitude FLOAT,
-    has_location CHAR(1),
-    
+    longitude NUMERIC(10, 7),
+    latitude NUMERIC(10, 7),
+    has_location BOOLEAN
 );
 
 -- Criação da tabela fact_brewery_operations
